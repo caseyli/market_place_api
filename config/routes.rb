4d3cc9bef8beef6b02368bf3e6277ac3f3b31225
Casey Li <casey.li@gmail.com>
2015-05-14 23:58:50 -0400

    Added show action for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-
   devise_for :users
+
   namespace :api, 
             defaults: { format: :json },
             constraints: { subdomain: 'api' },
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
 
     scope module: :v1,
                   constraints: ApiConstraints.new(version: 1, default: true) do
-      
+    
+      resources :users, only: [:show]  
     end
 
   end
