52e0a6ce6c170a50d3d1aaa8b09b321a9d798f6a
Casey Li <casey.li@gmail.com>
2015-05-15 00:23:44 -0400

    Adds destroy action to the users controller.

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -91,6 +91,15 @@ describe Api::V1::UsersController do
       end
     end
 
+    describe "DELETE #destroy" do
+      before(:each) do
+        @user = create :user
+        delete :destroy, { id: @user.id }, format: :json
+      end
+
+      it { should respond_with 204 }
+    end
+
   end
 
 end
